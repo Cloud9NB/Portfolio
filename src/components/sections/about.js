a082c3ad9173d0fdc4951bc01ce0869284cf6bc2
Kevin Keonhee Lee <blackseeds10@hotmail.com>
2022-06-08 20:08:04 -0700

    changes the about me

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -125,7 +125,16 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = [
+    'JavaScript (ES6+)',
+    'React',
+    'Express.js',
+    'Node.js',
+    'PostgreSQL',
+    'HTML',
+    'CSS',
+    'JQuery',
+  ];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -135,30 +144,17 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello! My name is Kevin and before I got to Web Development, I was working in the
+              construction industry. I knew that this wasn't something I wanted to continue into the
+              future especially on the wear it does to the body. I gotten in contact with some
+              friends in different industries, one which was a Web Developer, who showed me what he
+              does and and HOW he does it. The things that can be accomplished with endless
+              possibilities was which sparked my interest and curiosity.
             </p>
 
             <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
-            </p>
-
-            <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              Fast-forward to today, and I’ve had the privilege to attend and graduate from{' '}
+              <a href="https://www.lighthouselabs.ca">Lighthouse Labs</a> as a Web Developer
             </p>
 
             <p>Here are a few technologies I’ve been working with recently:</p>
@@ -173,7 +169,7 @@ const About = () => {
           <div className="wrapper">
             <StaticImage
               className="img"
-              src="../../images/me.jpg"
+              src="../../images/me.jpeg"
               width={500}
               quality={95}
               formats={['AUTO', 'WEBP', 'AVIF']}
